2f12b9d2c249486e5641b832477d3966b5a149f2
SouravCodes1080 <107361153+souravcodes1080@users.noreply.github.com>
2024-04-29 17:53:08 +0530

    gemini project

--- src/components/project/Middle.jsx
+++ src/components/project/Middle.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import hh from "../../assets/hh.png";
+import gemini from "../../assets/gemini.png";
 const Middle = () => {
   return (
 <div className="projects__content">
@@ -9,14 +9,14 @@ const Middle = () => {
             
         <img src={hh} alt="aboutImg" className="" width={"100%"} loading='lazy'/>
 
-        <h3>Hungry Helper</h3>
+        <h3>Gemini Clone</h3>
         <p>
-        This is a Food Ordering Web App build using Angular TS.
+          Gemini Clone is buit using React JS and Google Gemini API.
         </p>
         <br />
         <div className="project__links">
-            <a href="https://sourav-hungry-helper.netlify.app/" className='button website-button' target='blank'>Website</a>
-            <a href="https://github.com/souravcodes1080/foodApp" className='website-button github-btn' target='blank'>Github</a>
+            <a href="https://sourav-gemini-clone.netlify.app/" className='button website-button' target='blank'>Website</a>
+            <a href="https://github.com/souravcodes1080/gemini-clone" className='website-button github-btn' target='blank'>Github</a>
         </div>
         
         </div>
